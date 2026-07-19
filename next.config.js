17e1afda4e538ba171e900b3581e60e71bdfc5f5
luizweb1 <gustavluiz2005@gmail.com>
2026-07-19 13:21:49 -0300

    Corrigir runtime do Prisma na StayCloud
    Gera o Prisma Client em src/generated e remove a dependência do arquivo WASM em pasta oculta.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  experimental: {
-    outputFileTracingIncludes: {
-      "/api/**/*": [
-        "./node_modules/.prisma/client/**/*",
-      ],
-    },
-  },
 };
 
 module.exports = nextConfig;
